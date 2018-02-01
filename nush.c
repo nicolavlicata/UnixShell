a49a9472c9ef74ee1c43289d88be363a74fc7a03
Nicola Licata <licata.n@husky.neu.edu>
2018-02-01 11:20:22 -0500

    Remove a few comments

--- nush.c
+++ nush.c
@@ -297,40 +297,18 @@ execute(char* cmd)
     }
 
     if ((cpid = fork())) {
-        // parent process
-       // printf("Parent pid: %d\n", getpid());
-       // printf("Parent knows child pid: %d\n", cpid);
-
-        // Child may still be running until we wait.
 
         int status;
         waitpid(cpid, &status, 0);
 	free_svec(tokens);
 	free(tokensArray);
 	return;
-
-       // printf("== executed program complete ==\n");
-
-       // printf("child returned with wait code %d\n", status);
-       // if (WIFEXITED(status)) {
-           // printf("child exited with exit code (or main returned) %d\n", WEXITSTATUS(status));
-       // }
     }
     else {
-        // child process
-       // printf("Child pid: %d\n", getpid());
-       // printf("Child knows parent pid: %d\n", getppid());
-	
-        // The argv array for the child.
-        // Terminated by a null pointer.
-
-       // printf("== executed program's output: ==\n");
     
         execvp(svec_get(tokens, 0), tokensArray);
 	free_svec(tokens);
 	free(tokensArray);
-	
-       // printf("Can't get here, exec only returns on error.");
     }
 }
 
